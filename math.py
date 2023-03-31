26785afe71be38358005228133e12de67b4d0a41
clarine <antibaclarine.com>
2023-03-31 16:15:32 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=(a+b)
+    return answer
+
+def minus(a,b):
+    answer=(a-b)
+    return answer
+
+def div(a,b):
+    answer=(a/b)
+    return answer
+
+def multiply(a,b):
+    answer=(a*b)
+    return answer
+
+def remain(a,b):
+        answer=(a%b)
+        return answer
